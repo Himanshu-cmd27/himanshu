fcd00c986c62810cf269adacf8210bd2016949d1
Himanshu-cmd27 <rox9953228119@gmail.com>
2025-06-10 20:14:22 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,3 +5,11 @@ public class Main {
         javax.swing.SwingUtilities.invokeLater(StudentForm::new);
     }
 }
+import ui.StudentForm;
+
+public class Main {
+    public static void main(String[] args) {
+        // Run UI in the Event Dispatch Thread
+        javax.swing.SwingUtilities.invokeLater(() -> new StudentForm());
+    }
+}
