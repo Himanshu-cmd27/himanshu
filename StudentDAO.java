6ae92217fde2ecf95399e9e4d6e775465e461569
Himanshu-cmd27 <rox9953228119@gmail.com>
2025-06-10 20:02:46 +0530

    Create StudentDAO.java

---
+++ StudentDAO.java
@@ -0,0 +1,4 @@
+if(name.isEmpty() || email.isEmpty() || ageText.isEmpty()) {
+    JOptionPane.showMessageDialog(this, "All fields are required.", "Input Error", JOptionPane.ERROR_MESSAGE);
+    return;
+}
